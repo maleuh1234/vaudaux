1b4b512523b40dcec221765e48f3a984e6cd24f6
maleuh1234 <maeldugue7@gmail.com>
2024-08-09 11:43:26 +0200

    setup frontend & plugin OK

--- vaudaux-site/src/main.js
+++ vaudaux-site/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/main.css'
+import './assets/output.css'
+import gsap from 'gsap';
 
 createApp(App).use(router).use(router).mount('#app')

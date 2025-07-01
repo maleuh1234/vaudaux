2d4f995f2df1b761dbd1d60fbdf6b97a11a36b0b
maleuh1234 <maeldugue7@gmail.com>
2025-07-01 13:01:44 +0200

    vaudaux vacances

--- vaudaux-site/src/views/HomeView.vue
+++ vaudaux-site/src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home-page-background">
       <div class="background-layer" :style="{ backgroundImage: `url(${defaultImageUrl})` }">
          <!-- <img class="logo-ephj" src="../assets/images-home/EPHJ-logo.png" alt=""> -->
-        <!--<img src="../assets/images-home/vaudauxbasetexte.png" alt=""> -->
+        <img src="../assets/images-home/vacancestext.png" alt="">
       </div>
       <div class="foreground-layer" ref="foregroundLayer"></div>      
       <!-- <h1 class="home-h1" ref="enTete">{{textes[currentLanguage].Title}}</h1> -->
@@ -14,7 +14,7 @@
 <script>
 import { ref, onMounted } from 'vue';
 import { gsap } from 'gsap';
-import defaultImageUrl from '@/assets/images-home/summer.jpg';
+import defaultImageUrl from '@/assets/images-home/vacancest.jpg';
 import imageUrl1 from '@/assets/images-home/0K6A1107.jpg';
 import imageUrl2 from '@/assets/images-home/0K6A9321.jpg';
 
